0ce161393815703cd76b59f1bead13b5d646c9a6
PEDROGRUNWALD97 <PEDRO.GRUNWALD@GMAIL.COM>
2023-06-20 14:56:59 -0300

    feat/initial_route

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,7 +2,7 @@ import { ImageBackground } from 'react-native'
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
 import { styled } from 'nativewind'
-
+import * as SecureStore from 'expo-secure-store'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -11,15 +11,25 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  // eslint-disable-next-line prettier/prettier
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<null | boolean>(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) {
     return <SplashScreen />
@@ -38,7 +48,10 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
