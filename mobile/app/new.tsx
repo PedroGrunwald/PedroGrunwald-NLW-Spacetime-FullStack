7dbcfa068a5003bc4ef8915b4442f9bbb89ad050
PEDROGRUNWALD97 <PEDRO.GRUNWALD@GMAIL.COM>
2023-06-28 14:08:52 -0300

    feat/newMemory_text_button

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { Switch, TouchableOpacity, View, Text } from 'react-native'
+import { Switch, TouchableOpacity, View, Text, ScrollView } from 'react-native'
 import NwlLogo from '../src/assets/nlw.spacetime-logo.svg'
 import { Link } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
+import { TextInput } from 'react-native-gesture-handler'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
   const [isPublic, setIsPublic] = useState(false)
   return (
-    <View
+    <ScrollView
       className="flex-1 px-8"
-      style={{ paddingBottom: bottom, paddingTop: top }}
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
       <View className="mt-4 flex-row items-center justify-between">
         <NwlLogo />
@@ -33,7 +34,29 @@ export default function NewMemory() {
           <Text className="font-body text-base text-gray-200">
             Tornar memória pública</Text>
         </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
+          <View className="flex-row items-center gap-2">
+            <Icon name="image" color="#FFF" />
+            <Text className="font-body text-sm text-gray-200">
+              Adicionar foto ou video de capa
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TextInput
+          multiline
+          className="p-0 font-body text-lg text-gray-50"
+          placeholderTextColor="#56565a"
+          placeholder="Fique livre para adicionar fotos,videos e relatos sobre essa experiencia"
+        />
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className=" rounded-full items-center self-end bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
+        </TouchableOpacity>
       </View>
-    </View>
+    </ScrollView>
   )
 }
